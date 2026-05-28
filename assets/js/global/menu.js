345043fc743d4197313f72b82397633d7fd82594
lucasdourado-dev <lucas9dourado@gmail.com>
2026-05-28 17:11:50 -0300

    base menu feita

---
+++ assets/js/global/menu.js
@@ -0,0 +1,9 @@
+const toggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+toggle.addEventListener('click', () => {
+
+    toggle.classList.toggle('active');
+    menu.classList.toggle('active');
+
+});
